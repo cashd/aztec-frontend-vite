255cb76e4b8717c496ebf9a960b2798bc0e36190
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-10 13:26:30 -0600

    correct config for localhost rpc

--- src/clients/wagmiClient.ts
+++ src/clients/wagmiClient.ts
@@ -1,17 +1,38 @@
-import { configureChains, createClient } from "wagmi";
-import { localhost, mainnet } from "wagmi/chains";
+import { Chain, configureChains, createClient, mainnet } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 const RPC_KEY = import.meta.env.VITE_RPC_KEY;
 
-// if (!RPC_KEY) {
-//   throw new Error("Provide an RPC_KEY variable in .env");
-// }
+if (!RPC_KEY) {
+  throw new Error("Provide an RPC_KEY variable in .env");
+}
+
+const _localhost: Chain = {
+  name: "Localhost",
+  network: "localhost",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  id: 1337,
+  rpcUrls: {
+    public: { http: ["http://localhost:8545/"] },
+    default: { http: ["http://localhost:8545/"] },
+  },
+};
 
 const { provider, webSocketProvider } = configureChains(
-  [mainnet, localhost],
-  [alchemyProvider({ apiKey: RPC_KEY }), publicProvider()]
+  [mainnet, _localhost],
+  [
+    alchemyProvider({ apiKey: RPC_KEY }),
+    jsonRpcProvider({
+      rpc: () => ({
+        http: `http://localhost:8545`,
+      }),
+    }),
+  ]
 );
 
 export const client = createClient({
