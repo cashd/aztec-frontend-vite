a0b4e81bdada06bdbda024bcaff414510062d1f7
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-10 13:25:52 -0600

    formatAddress util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(" ");
 }
+
+export function formatAddress(address: string): string {
+  return `0x${address.slice(2, 4)}...${address.slice(-4)}`;
+}
